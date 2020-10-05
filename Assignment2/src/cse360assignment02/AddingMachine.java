18cf0fd0ac551e41691469f98d5dd7f5a75333e8
Jenny Z <jzhan444@asu.edu>
2020-10-05 11:48:05 -0700

    Initial CSE 360 assignment 2 code

---
+++ Assignment2/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {		//main function
+  private int total;				//create variable
+  
+  public AddingMachine () {
+    total = 0;  					// not needed - included for clarity
+  }
+  
+  public int getTotal () {			//function for returning the total
+    return 0;						//always return 0
+  }
+  
+  public void add (int value) {		//function for adding, does not return
+  }
+
+  public void subtract (int value) {//function for subtracting, does not return
+  }
+
+  public String toString () {		//concatenate strings
+    return "";
+  }
+
+  public void clear() {				
+  }
+}
